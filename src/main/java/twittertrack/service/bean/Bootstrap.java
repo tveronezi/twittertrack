6e6be8098b147a6334449a7b9e8d675f7cd70cf7
tveronezi <thiago@veronezi.org>
2014-06-27 10:07:06 -0400

    init

--- src/main/java/twittertrack/service/bean/Bootstrap.java
+++ src/main/java/twittertrack/service/bean/Bootstrap.java
@@ -20,10 +20,17 @@ package twittertrack.service.bean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import twittertrack.service.ApplicationException;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Singleton
 @Startup
@@ -31,9 +38,39 @@ public class Bootstrap {
 
     private final Logger log = LoggerFactory.getLogger("twittertrack");
 
+    private static final File DEFAULT_TWITTER_PROPS = new File(System.getProperty("user.home"), "twitter.properties");
+    private static final String TWITTER_PROPS = System.getProperty(
+            "twitter_properties", DEFAULT_TWITTER_PROPS.getPath()
+    );
+
+    private static final String TWITTER_API_KEY = "api_key";
+    private static final String TWITTER_API_SECRET = "api_secret";
+    private Properties twitterProperties;
+
     @PostConstruct
     public void prepare() {
         log.debug("Starting application...");
+        try (final InputStream fis = new FileInputStream(TWITTER_PROPS)) {
+            this.twitterProperties = new Properties();
+            this.twitterProperties.load(fis);
+        } catch (FileNotFoundException e) {
+            log.error("System property 'twitter_properties' and '{}' not found.", DEFAULT_TWITTER_PROPS.getPath());
+            throw new ApplicationException(e);
+        } catch (IOException e) {
+            throw new ApplicationException(e);
+        }
+        validateTwitterProperties();
+    }
+
+    private void validateTwitterProperties() {
+        if (this.twitterProperties.getProperty(TWITTER_API_KEY) == null) {
+            log.error("Your '{}' file does not contain the {} property.", TWITTER_PROPS, TWITTER_API_KEY);
+            throw new ApplicationException("Bad twitter properties file.");
+        }
+        if (this.twitterProperties.getProperty(TWITTER_API_SECRET) == null) {
+            log.error("Your '{}' file does not contain the {} property.", TWITTER_PROPS, TWITTER_API_KEY);
+            throw new ApplicationException("Bad twitter properties file.");
+        }
     }
 
 }
