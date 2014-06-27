7cd28d437727c22a550a3dbbf62bd4d39126a17c
tveronezi <thiago@veronezi.org>
2014-06-27 08:33:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+twittertrack
+=========
+
+### Quick start ###
+
+You can run the application by following these simple steps.
+
+1. Checkout this source code
+2. Go to the root of your application and run *mvn clean install tomee:run*
+3. Open <http://localhost:8080/twittertrack>
+
+### Dependency ###
+
+1. >= Maven 3.x.x 
+2. >= Java 1.6
---
+++ src/main/webapp/app/js/application.js
@@ -0,0 +1,63 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+(function () {
+    'use strict';
+
+    var deps = [
+        'app/js/view/container',
+        'lib/underscore',
+        'app/js/i18n',
+        'app/js/keep-alive',
+        'lib/less', 'lib/backbone', 'lib/jquery'
+    ];
+    define(deps, function (containerView, underscore, i18n, ping) {
+        $.ajaxSetup({ cache: false });
+
+        function start() {
+            //Starting the backbone router.
+            var Router = Backbone.Router.extend({
+                routes: {
+                    '': 'home'
+                }
+            });
+            var router = new Router();
+            router.on("route:home", function () {
+                containerView.render();
+            });
+
+            //Starting the backbone history.
+            Backbone.history.start({
+                pushState: true,
+                root: window.ux.ROOT_URL // This value is set by <c:url>
+            });
+
+            ping.start();
+
+            return {
+                getRouter: function () {
+                    return router;
+                }
+            };
+        }
+
+        return {
+            start: start
+        };
+    });
+}());
